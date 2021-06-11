044f8a04d212dc40411b1a5ca3293682e994d8d4
imaginelenses <imaginelenses@gmail.com>
2021-06-11 18:42:16 +0530

    Validate name

--- contacts.py
+++ contacts.py
@@ -3,3 +3,21 @@
 """
     Take name and phone number from user ad store it in a phone book
 """
+
+# Get fullname from user
+while True:
+    full_name = input("Enter fullname: ")
+
+    # Split fullname into separate names
+    names = full_name.split(' ')
+
+    # Validate name
+    stop = True
+    for name in names:
+        if not name.isalpha():
+            stop = False
+            break
+
+    if stop:
+        break
+
